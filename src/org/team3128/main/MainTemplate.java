5ebf18d7c848c894aa4ef8ec7651a57f1bfbcc7f
Ronak Roy <ronak.r123@gmail.com>
2018-01-06 11:57:29 -0800

    Created Project

---
+++ src/org/team3128/main/MainTemplate.java
@@ -0,0 +1,49 @@
+package org.team3128.main;
+
+import org.team3128.common.NarwhalRobot;
+import org.team3128.common.util.GenericSendableChooser;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class MainTemplate extends NarwhalRobot 
+{
+	
+	@Override
+	protected void constructHardware() 
+	{
+		
+	}
+
+	@Override
+	protected void setupListeners() {
+		
+	}
+
+	@Override
+	protected void teleopInit() {
+
+	}
+	
+	@Override
+	protected void autonomousInit() {
+
+	}
+	
+	protected void constructAutoPrograms(GenericSendableChooser<CommandGroup> programChooser)
+	{		
+
+	}
+	
+	@Override
+	protected void updateDashboard() {
+		
+	}
+
+
+	@Override
+	protected void teleopPeriodic()
+	{
+		
+	}
+	
+}
