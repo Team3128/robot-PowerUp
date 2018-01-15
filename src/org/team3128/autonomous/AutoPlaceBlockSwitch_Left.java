860e6937212b109084b1eed1f98926aeedb86846
googolplex8 <googolplex8@yahoo.com>
2018-01-15 12:42:19 -0800

    revised auto code

--- src/org/team3128/autonomous/AutoPlaceBlockSwitch_Left.java
+++ src/org/team3128/autonomous/AutoPlaceBlockSwitch_Left.java
@@ -14,40 +14,39 @@ import org.team3128.mechanisms.Intake;
 
 public class AutoPlaceBlockSwitch_Left extends CommandGroup {
 	public AutoPlaceBlockSwitch_Left(SRXTankDrive drive, Forklift forklift) {
-		final double autoLineDistance = 160 * Length.in; // distance between startPos and autoLine
-		final double switchDistance = 168 * Length.in; // distance between startPos and switch
-		final double horizOffset = 10 * Length.in; // distance between start position horizontal and switch horizontal
-		final double switchOffset = 100 * Length.in; // robot width
+
+		AutoPlaceBlockSwitch autoValues = new AutoPlaceBlockSwitch();
 		
+		final double switchDistance = autoValues.switchDistance; // vertical distance between startPos and switch
+		final double horizOffset = autoValues.horizOffset; // distance between start position horizontal and switch horizontal
+		final double switchWidth = autoValues.switchDistance; // width of the switch
+
 		String gameData;
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
-		
+
 		if (gameData.charAt(0) == 'L') {
-			addSequential(drive.new CmdMoveForward(autoLineDistance, 4000, 0.75)); // drive forward to autoline
-			Log.info("MainGuido(AUTO)", "[1]reached autoLine");
-			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.LEFT)); // turn left
-			addSequential(drive.new CmdMoveForward(horizOffset, 2000, 0.75)); // compensate for horizontal offset
+			addSequential(drive.new CmdMoveForward(switchDistance, 4000, 0.75)); // move to switch vertical
+			Log.info("MainGuido(AUTO)", "[1]reached switch vertical");
+
 			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.RIGHT)); // turn right
-			addSequential(drive.new CmdMoveForward(switchDistance - autoLineDistance, 2000, 0.75)); // move to switch
-			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.RIGHT)); // move to right position(turn
-																						// right)
-			Log.info("MainGuido(AUTO)", "[2]reached target position");
-			// forklift.setState(Forklift.State.SWITCH, Intake.State.OUTTAKE); //push block
-			// on to switch
+			addSequential(drive.new CmdMoveForward(horizOffset, 2000, 0.75)); // move to switch horizontal
+			Log.info("MainGuido(AUTO)", "[2]reached switch horizontal");
+
 			addSequential(forklift.new CmdForkliftPush(Forklift.State.SWITCH, Intake.intakeState.OUTTAKE));
 			Log.info("MainGuido(AUTO)", "[3]outtake activated");
 
 		} else if (gameData.charAt(0) == 'R') {
-			addSequential(drive.new CmdMoveForward(autoLineDistance, 4000, 0.75)); // drive forward to autoline
-			Log.info("MainGuido(AUTO)", "[1]reached autoLine");
+			addSequential(drive.new CmdMoveForward(switchDistance / 2, 4000, 0.75)); // drive forward
 			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.RIGHT)); // turn right
-			addSequential(drive.new CmdMoveForward(switchOffset, 2000, 0.75)); // compensate for horizontal offset
+			addSequential(drive.new CmdMoveForward(horizOffset * 2 + switchWidth, 2000, 0.75)); // across field
 			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.LEFT)); // turn left
-			addSequential(drive.new CmdMoveForward(switchDistance - autoLineDistance, 2000, 0.75)); // move to switch
+			addSequential(drive.new CmdMoveForward(switchDistance / 2, 2000, 0.75)); // move to switch vertical
+			Log.info("MainGuido(AUTO)", "[1]reached switch vertical");
+
 			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.LEFT)); // turn left
-			Log.info("MainGuido(AUTO)", "[2]reached target position");
-			// forklift.setState(Forklift.State.SWITCH, Intake.State.OUTTAKE); //push block
-			// on to switch
+			addSequential(drive.new CmdMoveForward(horizOffset, 2000, 0.75)); // move to switch horizontal
+			Log.info("MainGuido(AUTO)", "[2]reached switch horizontal");
+
 			addSequential(forklift.new CmdForkliftPush(Forklift.State.SWITCH, Intake.intakeState.OUTTAKE));
 			Log.info("MainGuido(AUTO)", "[3]outtake activated");
 		}
--- src/org/team3128/autonomous/AutoPlaceBlockSwitch_Middle.java
+++ src/org/team3128/autonomous/AutoPlaceBlockSwitch_Middle.java
@@ -2,6 +2,8 @@ package org.team3128.autonomous;
 
 import org.team3128.common.autonomous.movement.CmdTurnGyro;
 import org.team3128.common.drive.SRXTankDrive;
+import org.team3128.common.drive.SRXTankDrive.CmdInPlaceTurn;
+import org.team3128.common.drive.SRXTankDrive.CmdMoveForward;
 import org.team3128.common.util.Log;
 import org.team3128.common.util.enums.Direction;
 import org.team3128.common.util.units.Length;
@@ -11,45 +13,47 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 import org.team3128.mechanisms.Forklift;
 import org.team3128.mechanisms.Intake;
+import org.team3128.mechanisms.Forklift.CmdForkliftPush;
 
 public class AutoPlaceBlockSwitch_Middle extends CommandGroup {
 	public AutoPlaceBlockSwitch_Middle(SRXTankDrive drive, Forklift forklift) {
-		final double autoLineDistance = 160 * Length.in; // distance between startPos and autoLine
-		final double switchDistance = 168 * Length.in; // distance between startPos and switch
-		final double horizOffset = 30 * Length.in; // distance between start position horizontal and switch horizontal
-		final double switchOffset = 100 * Length.in; // robot width
 		
+		AutoPlaceBlockSwitch autoValues = new AutoPlaceBlockSwitch();
+		
+		final double switchDistance = autoValues.switchDistance; // vertical distance between startPos and switch
+		final double horizOffset = autoValues.horizOffset; // distance between start position horizontal and switch horizontal
+		final double switchWidth = autoValues.switchDistance; // width of the switch
+
 		String gameData;
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
-		
-		if (gameData.charAt(0) == 'L') {
-			addSequential(drive.new CmdMoveForward(switchDistance / 2, 4000, 0.75)); // drive forward to autoline
-			Log.info("MainGuido(AUTO)", "[1]reached autoLine");
-			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.LEFT)); // turn left
-			addSequential(drive.new CmdMoveForward(horizOffset, 2000, 0.75)); // compensate for horizontal offset
+
+		if (gameData.charAt(0) == 'R') {
+			addSequential(drive.new CmdMoveForward(switchDistance / 2, 4000, 0.75)); // drive forward
 			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.RIGHT)); // turn right
-			addSequential(drive.new CmdMoveForward(switchDistance / 2, 4000, 0.75)); // drive forward to autoline
-			addSequential(drive.new CmdMoveForward(switchDistance - autoLineDistance, 2000, 0.75)); // move to switch
-			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.RIGHT)); // move to right position(turn
-																						// right)
-			Log.info("MainGuido(AUTO)", "[2]reached target position");
-			// forklift.setState(Forklift.State.SWITCH, Intake.State.OUTTAKE); //push block
-			// on to switch
+			addSequential(drive.new CmdMoveForward(horizOffset + switchWidth/2, 2000, 0.75)); // across field
+			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.LEFT)); // turn left
+			addSequential(drive.new CmdMoveForward(switchDistance / 2, 2000, 0.75)); // move to switch vertical
+			Log.info("MainGuido(AUTO)", "[1]reached switch vertical");
+
+			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.LEFT)); // turn left
+			addSequential(drive.new CmdMoveForward(horizOffset, 2000, 0.75)); // move to switch horizontal
+			Log.info("MainGuido(AUTO)", "[2]reached switch horizontal");
+
 			addSequential(forklift.new CmdForkliftPush(Forklift.State.SWITCH, Intake.intakeState.OUTTAKE));
 			Log.info("MainGuido(AUTO)", "[3]outtake activated");
 
-		} else if (gameData.charAt(0) == 'R') {
-			addSequential(drive.new CmdMoveForward(switchDistance / 2, 4000, 0.75)); // drive forward to autoline
-			Log.info("MainGuido(AUTO)", "[1]reached autoLine");
+		} else if (gameData.charAt(0) == 'L') {
+			addSequential(drive.new CmdMoveForward(switchDistance / 2, 4000, 0.75)); // drive forward
+			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.LEFT)); // turn left
+			addSequential(drive.new CmdMoveForward(horizOffset + switchWidth/2, 2000, 0.75)); // across field
 			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.RIGHT)); // turn right
-			addSequential(drive.new CmdMoveForward(switchOffset, 2000, 0.75)); // compensate for horizontal offset
-			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.LEFT)); // turn left
-			addSequential(drive.new CmdMoveForward(switchDistance / 2, 4000, 0.75)); // drive forward to autoline
-			addSequential(drive.new CmdMoveForward(switchDistance - autoLineDistance, 2000, 0.75)); // move to switch
-			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.LEFT)); // turn left
-			Log.info("MainGuido(AUTO)", "[2]reached target position");
-			// forklift.setState(Forklift.State.SWITCH, Intake.State.OUTTAKE); //push block
-			// on to switch
+			addSequential(drive.new CmdMoveForward(switchDistance / 2, 2000, 0.75)); // move to switch vertical
+			Log.info("MainGuido(AUTO)", "[1]reached switch vertical");
+
+			addSequential(drive.new CmdInPlaceTurn(90, 0.75, 1500, Direction.RIGHT)); // turn right
+			addSequential(drive.new CmdMoveForward(horizOffset, 2000, 0.75)); // move to switch horizontal
+			Log.info("MainGuido(AUTO)", "[2]reached switch horizontal");
+
 			addSequential(forklift.new CmdForkliftPush(Forklift.State.SWITCH, Intake.intakeState.OUTTAKE));
 			Log.info("MainGuido(AUTO)", "[3]outtake activated");
 		}
